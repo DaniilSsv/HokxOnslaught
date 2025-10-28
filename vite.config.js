b10875abef863f97f1728cfd3ca3e6d521353860
DaniilSsv <daniil1samsonov@gmail.com>
2025-10-28 14:24:15 +0100

    pages fix try 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-// If deploying to GitHub Pages under https://<user>.github.io/HokxOnslaught/
-// set `base` to the repository name with leading and trailing slashes.
-// This ensures built asset URLs point to /HokxOnslaught/... instead of /... which 404s on GitHub Pages.
 export default defineConfig({
-  base: 'https://kaljmarik.github.io/HokxOnslaught/'
-})
+    base: "/HokxOnslaught/",
+});
